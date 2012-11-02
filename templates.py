56832220459853b7feac872623179403e0f19797
Luke Jolly <jollygamer321@gmail.com>
2012-11-02 15:16:17 -0400

    Added templates.py

---
+++ templates.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python2.6
+
+link="""[$name] $ips
+    RX  bytes: $rx_bytes
+        packets: $rx_packets
+        errors:$rx_errors dropped:$rx_dropped overrun:$rx_overrun mcast:$rx_mcast
+    TX  bytes: $tx_bytes
+        packets: $tx_packets
+        errors:$tx_errors dropped:$tx_dropped carrier:$tx_carrier collsns:$tx_collsns"""
