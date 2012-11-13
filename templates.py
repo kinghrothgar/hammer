ad049122b1a87e5c034d93c8f6e40aa0c7c953fa
Luke Jolly <jollygamer321@gmail.com>
2012-11-13 01:10:04 -0500

    Statistics tcpv done

--- templates.py
+++ templates.py
@@ -43,3 +43,94 @@ IpExt:
     InMcastOctets: $InMcastOctets ($InMcastOctets_abso)
     OutMcastOctets: $OutMcastOctets ($OutMcastOctets_abso)
     InBcastOctets: $InBcastOctets ($InBcastOctets_abso)"""
+
+tcpv="""Tcp:
+    $activeconnectionsopenings ($activeconnectionsopenings_abso) active connections openings
+    $passiveconnectionopenings ($passiveconnectionopenings_abso) passive connection openings
+    $failedconnectionattempts ($failedconnectionattempts_abso) failed connection attempts
+    $connectionresetsreceived ($connectionresetsreceived_abso) connection resets received
+    $connectionsestablished ($connectionsestablished_abso) connections established
+    $segmentsreceived ($segmentsreceived_abso) segments received
+    $segmentssendout ($segmentssendout_abso) segments send out
+    $segmentsretransmited ($segmentsretransmited_abso) segments retransmited
+    $badsegmentsreceived ($badsegmentsreceived_abso) bad segments received.
+    $resetssent ($resetssent_abso) resets sent
+TcpExt:
+    $SYNcookiessent ($SYNcookiessent_abso) SYN cookies sent
+    $SYNcookiesreceived ($SYNcookiesreceived_abso) SYN cookies received
+    $invalidSYNcookiesreceived ($invalidSYNcookiesreceived_abso) invalid SYN cookies received
+    $resetsreceivedforembryonicSYNRECVsockets ($resetsreceivedforembryonicSYNRECVsockets_abso) resets received for embryonic SYN_RECV sockets
+    $packetsprunedfromreceivequeuebecauseofsocketbufferoverrun ($packetsprunedfromreceivequeuebecauseofsocketbufferoverrun_abso) packets pruned from receive queue because of socket buffer overrun
+    $packetsprunedfromreceivequeue ($packetsprunedfromreceivequeue_abso) packets pruned from receive queue
+    $packetsdroppedfromoutoforderqueuebecauseofsocketbufferoverrun ($packetsdroppedfromoutoforderqueuebecauseofsocketbufferoverrun_abso) packets dropped from out-of-order queue because of socket buffer overrun
+    $ICMPpacketsdroppedbecausetheywereoutofwindow ($ICMPpacketsdroppedbecausetheywereoutofwindow_abso) ICMP packets dropped because they were out-of-window
+    $ICMPpacketsdroppedbecausesocketwaslocked ($ICMPpacketsdroppedbecausesocketwaslocked_abso) ICMP packets dropped because socket was locked
+    $TCPsocketsfinishedtimewaitinfasttimer ($TCPsocketsfinishedtimewaitinfasttimer_abso) TCP sockets finished time wait in fast timer
+    $timewaitsocketsrecycledbytimestamp ($timewaitsocketsrecycledbytimestamp_abso) time wait sockets recycled by time stamp
+    $TCPsocketsfinishedtimewaitinslowtimer ($TCPsocketsfinishedtimewaitinslowtimer_abso) TCP sockets finished time wait in slow timer
+    $packetsrejectsinestablishedconnectionsbecauseoftimestamp ($packetsrejectsinestablishedconnectionsbecauseoftimestamp_abso) packets rejects in established connections because of timestamp
+    $delayedackssent ($delayedackssent_abso) delayed acks sent
+    $delayedacksfurtherdelayedbecauseoflockedsocket ($delayedacksfurtherdelayedbecauseoflockedsocket_abso) delayed acks further delayed because of locked socket
+    Quick ack mode was activated $Quickackmodewasactivatedtimes ($Quickackmodewasactivatedtimes_abso) times
+    $timesthelistenqueueofasocketoverflowed ($timesthelistenqueueofasocketoverflowed_abso) times the listen queue of a socket overflowed
+    $SYNstoLISTENsocketsignored ($SYNstoLISTENsocketsignored_abso) SYNs to LISTEN sockets ignored
+    $packetsdirectlyqueuedtorecvmsgprequeue ($packetsdirectlyqueuedtorecvmsgprequeue_abso) packets directly queued to recvmsg prequeue.
+    $bytesdirectlyreceivedinprocesscontextfromprequeue ($bytesdirectlyreceivedinprocesscontextfromprequeue_abso) bytes directly received in process context from prequeue
+    $packetsdirectlyreceivedfrombacklog ($packetsdirectlyreceivedfrombacklog_abso) packets directly received from backlog
+    $packetsdirectlyreceivedfromprequeue ($packetsdirectlyreceivedfromprequeue_abso) packets directly received from prequeue
+    $packetsdroppedfromprequeue ($packetsdroppedfromprequeue_abso) packets dropped from prequeue
+    $packetsheaderpredicted ($packetsheaderpredicted_abso) packet headers predicted
+    $packetheaderspredicted ($packetheaderspredicted_abso) packet headers predicted
+    $packetsheaderpredictedanddirectlyqueuedtouser ($packetsheaderpredictedanddirectlyqueuedtouser_abso) packets header predicted and directly queued to user
+    $acknowledgmentsnotcontainingdatareceived ($acknowledgmentsnotcontainingdatareceived_abso) acknowledgments not containing data payload received
+    $acknowledgmentsnotcontainingdatapayloadreceived ($acknowledgmentsnotcontainingdatapayloadreceived_abso) acknowledgments not containing data payload received
+    $predictedacknowledgments ($predictedacknowledgments_abso) predicted acknowledgments
+    $timesrecoveredfrompacketlossbyselectiveacknowledgements ($timesrecoveredfrompacketlossbyselectiveacknowledgements_abso) times recovered from packet loss by selective acknowledgements
+    $timesrecoveredfrompacketlossduetofastretransmit ($timesrecoveredfrompacketlossduetofastretransmit_abso) times recovered from packet loss due to fast retransmit
+    $timesrecoveredfrompacketlossduetoSACKdata ($timesrecoveredfrompacketlossduetoSACKdata_abso) times recovered from packet loss due to SACK data
+    $badSACKsreceived ($badSACKsreceived_abso) bad SACKs received
+    Detected reordering $DetectedreorderingtimesusingFACK ($DetectedreorderingtimesusingFACK_abso) times using FACK
+    Detected reordering $DetectedreorderingtimesusingSACK ($DetectedreorderingtimesusingSACK_abso) times using SACK
+    Detected reordering $Detectedreorderingtimesusingrenofastretransmit ($Detectedreorderingtimesusingrenofastretransmit_abso) times using reno fast retransmit
+    Detected reordering $Detectedreorderingtimesusingtimestamp ($Detectedreorderingtimesusingtimestamp_abso) times using time stamp
+    $congestionwindowsfullyrecovered ($congestionwindowsfullyrecovered_abso) congestion windows fully recovered
+    $congestionwindowspartiallyrecoveredusingHoeheuristic ($congestionwindowspartiallyrecoveredusingHoeheuristic_abso) congestion windows partially recovered using Hoe heuristic
+    TCPDSACKUndo: $TCPDSACKUndo ($TCPDSACKUndo_abso)
+    $congestionwindowsrecoveredwithoutslowstartbyDSACK ($congestionwindowsrecoveredwithoutslowstartbyDSACK_abso) congestion windows recovered without slow start by DSACK
+    $congestionwindowsrecoveredwithoutslowstartafterpartialack ($congestionwindowsrecoveredwithoutslowstartafterpartialack_abso) congestion windows recovered without slow start after partial ack
+    $congestionwindowsrecoveredafterpartialack ($congestionwindowsrecoveredafterpartialack_abso) congestion windows recovered after partial ack
+    $TCPdatalossevents ($TCPdatalossevents_abso) TCP data loss events
+    TCPLostRetransmit: $TCPLostRetransmit ($TCPLostRetransmit_abso)
+    $timeoutsafterrenofastretransmit ($timeoutsafterrenofastretransmit_abso) timeouts after reno fast retransmit
+    $timeoutsafterSACKrecovery ($timeoutsafterSACKrecovery_abso) timeouts after SACK recovery
+    $timeoutsinlossstate ($timeoutsinlossstate_abso) timeouts in loss state
+    $fastretransmits ($fastretransmits_abso) fast retransmits
+    $forwardretransmits ($forwardretransmits_abso) forward retransmits
+    $retransmitsinslowstart ($retransmitsinslowstart_abso) retransmits in slow start
+    $otherTCPtimeouts ($otherTCPtimeouts_abso) other TCP timeouts
+    TCPRenoRecoveryFail: $TCPRenoRecoveryFail ($TCPRenoRecoveryFail_abso)
+    $sackretransmitsfailed ($sackretransmitsfailed_abso) sack retransmits failed
+    $timesreceiverscheduledtoolatefordirectprocessing ($timesreceiverscheduledtoolatefordirectprocessing_abso) times receiver scheduled too late for direct processing
+    $packetscollapsedinreceivequeueduetolowsocketbuffer ($packetscollapsedinreceivequeueduetolowsocketbuffer_abso) packets collapsed in receive queue due to low socket buffer
+    $DSACKssentforoldpackets ($DSACKssentforoldpackets_abso) DSACKs sent for old packets
+    $DSACKssentforoutoforderpackets ($DSACKssentforoutoforderpackets_abso) DSACKs sent for out of order packets
+    $DSACKsreceived ($DSACKsreceived_abso) DSACKs received
+    $DSACKsforoutoforderpacketsreceived ($DSACKsforoutoforderpacketsreceived_abso) DSACKs for out of order packets received
+    $connectionsresetduetounexpectedSYN ($connectionsresetduetounexpectedSYN_abso) connections reset due to unexpected SYN
+    $connectionsresetduetounexpecteddata ($connectionsresetduetounexpecteddata_abso) connections reset due to unexpected data
+    $connectionsresetduetoearlyuserclose ($connectionsresetduetoearlyuserclose_abso) connections reset due to early user close
+    $connectionsabortedduetotimeout ($connectionsabortedduetotimeout_abso) connections aborted due to timeout
+    $timesunabledtosendRSTduetonomemory ($timesunabledtosendRSTduetonomemory_abso) times unabled to send RST due to no memory
+    TCP ran low on memory $TCPranlowonmemorytimes ($TCPranlowonmemorytimes_abso) times
+    TCPSACKDiscard: $TCPSACKDiscard ($TCPSACKDiscard_abso) 
+    TCPDSACKIgnoredOld: $TCPDSACKIgnoredOld ($TCPDSACKIgnoredOld_abso) 
+    TCPDSACKIgnoredNoUndo: $TCPDSACKIgnoredNoUndo ($TCPDSACKIgnoredNoUndo_abso) 
+    TCPSpuriousRTOs: $TCPSpuriousRTOs ($TCPSpuriousRTOs_abso) 
+    TCPSackShifted: $TCPSackShifted ($TCPSackShifted_abso) 
+    TCPSackMerged: $TCPSackMerged ($TCPSackMerged_abso) 
+    TCPSackShiftFallback: $TCPSackShiftFallback ($TCPSackShiftFallback_abso) 
+    TCPBacklogDrop: $TCPBacklogDrop ($TCPBacklogDrop_abso)
+    TCPRcvCoalesce: $TCPRcvCoalesce ($TCPRcvCoalesce_abso)
+    TCPOFOQueue: $TCPOFOQueue ($TCPOFOQueue_abso)
+    TCPChallengeACK: $TCPChallengeACK ($TCPChallengeACK_abso)
+    TCPSYNChallenge: $TCPSYNChallenge ($TCPSYNChallenge_abso)"""
